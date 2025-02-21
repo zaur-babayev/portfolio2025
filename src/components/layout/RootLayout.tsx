502403d69fc472b1f99f6fd8b2cd073a63059766
zaur-babayev <mail@zaurb.com>
2025-02-21 14:17:53 +0200

    Footer message

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -33,7 +33,7 @@ const RootLayout = () => {
           <div className="flex flex-col items-center gap-6">
             <Socials />
             <p className="text-sm text-muted-foreground">
-              {new Date().getFullYear()} • Zaur Babayev all rights reserved
+              {new Date().getFullYear()} • Zaur Babayev all rights reserved 😎
             </p>
           </div>
         </div>
